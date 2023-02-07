c62aabdcb349425c0e3b3a596f5a8196c93913f1
abinash <dj.avinash55@gmail.com>
2023-02-07 21:45:55 +0530

    my 1st commit addition

---
+++ src/test/java/Git/Addition.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=1;
+		int b=2;
+		
+		int c=a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
